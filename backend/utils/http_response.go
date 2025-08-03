a34d904c4c19c093b331858cb4a6900a352e5ab1
Kocannn <dwicandraandika4@gmail.com>
2025-08-04 00:36:35 +0800

    feat(utils): add HTTP header and response helpers
    Add utility functions for extracting Bearer tokens from HTTP headers and
    sending standardized JSON responses. Improves code reuse and error
    handling
    for HTTP endpoints.

---
+++ backend/utils/http_response.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/Kocannn/self-dunking-ai/domain"
+)
+
+func Response(data domain.HttpResponse, w http.ResponseWriter) {
+	// Marshal the data to JSON
+	response, err := json.Marshal(data)
+	if err != nil {
+		// Handle error in case of failed JSON marshaling
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Internal server error"}`))
+		return
+	}
+
+	// Set Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
+	// Set the status code as provided
+	w.WriteHeader(data.Code)
+	// Write the JSON data
+	w.Write(response)
+}
